0752053cd8780b6fc6e1ed34d2bb2a50b870b724
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-09 12:52:17 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+hospital = Hospital.create(name: 'Grey Sloan Memorial Hospital')
+
+doctor1 = hospital.doctors.create(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard')
+doctor2 = hospital.doctors.create(name: 'Alex Karev', specialty: 'Pediatric Surgery', university: 'Johns Hopkins') 
+doctor3 = hospital.doctors.create(name: 'Miranda Bailey', specialty: 'General Surgery', university: 'Harvard') 
+
+patient1 = doctor1.patients.create!(name: 'Denny Duquette', age: 39)
+patient2 = doctor1.patients.create!(name: 'Rebecca Pope', age: 32)
+patient3 = doctor1.patients.create!(name: 'Zola Shepherd', age: 2)
+
+
