904edab19c31f2ada8a7156d0b6522a351bb5b6c
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-09 12:26:24 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :doctors, only: [:show]
+
+  namespace :doctors, only: [:show] do
+    resources :patients, only: [:destroy]
+  end
+
+  # delete 'doctors/:id', to: 'doctors#destroy_patient'
   resources :hospitals, only: [:show]
 end
