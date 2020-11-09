8e88c318796f1fb64341e27a6f6d17e3cf923112
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-09 11:45:19 -0600

    Create number_of_doctors and unique_universities model methods

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,11 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def number_of_doctors
+    doctors.count
+  end
+
+  def unique_universities
+    doctors.select(:university).distinct.pluck(:university)
+  end
 end
