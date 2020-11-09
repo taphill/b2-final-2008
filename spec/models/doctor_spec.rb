bc7bd25f8069fa265741684bfc5f64d58b6bbeeb
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-09 11:14:25 -0600

    Add test for hopital_name method in the Doctor model

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Doctor, type: :model do
     it { should have_many :doctor_patients }
     it { should have_many(:patients).through(:doctor_patients) }
   end
+
+  describe 'instance methods' do
+    context '#hopital_name' do
+      it 'should return the doctors hospital' do
+        hospital = Hospital.create(name: 'Grey Sloan Memorial Hospital') 
+        doctor = hospital.doctors.create(name: 'Meredith Grey', specialty: 'General Surgery', university: 'Harvard') 
+
+        expect(doctor.hospital_name).to eq('Grey Sloan Memorial Hospital')
+      end
+    end
+  end
 end
